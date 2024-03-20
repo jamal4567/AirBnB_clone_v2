2feb9279723e9cbf8cb9705de4b94a145bcf7319
Marvel paul <djmarvel@yahoo.com>
2024-03-20 10:42:38 -0700

    about to checkout

---
+++ models/engine/db_storage.py
@@ -0,0 +1,39 @@
+"""Defines the DBStorage engine."""
+
+from sqlalchemy import create_engine
+from os import getenv
+
+
+class DBStorage:
+    """Represents a database storage engine.
+
+    Attributes:
+        __engine (sqlalchemy.Engine): The working SQLAlchemy engine.
+        __session (sqlalchemy.Session): The working SQLAlchemy session.
+    """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """
+        Initialize a new DBStorage instance.
+        """
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")),
+                                      pool_pre_ping=True)
+
+        if getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query on the curret database session all objects of the given class.
+
+        If cls is None, queries all types of objects.
+
+        Return:
+            Dict of queried classes in the format <class name>.<obj id> = obj.
+        """
+
